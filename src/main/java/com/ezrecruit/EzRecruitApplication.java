493ea4a93297992262a69b6188f6020e216cf24f
Sangyeop Kim <sangykimiv@gmail.com>
2024-02-14 11:29:38 +0900

    init project

---
+++ src/main/java/com/ezrecruit/EzRecruitApplication.java
@@ -0,0 +1,13 @@
+package com.ezrecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzRecruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzRecruitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezrecruit/EzRecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezrecruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzRecruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
